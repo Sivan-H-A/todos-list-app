6ecbcd22fa77849961f19387a49040e6994ab023
Sivan-H-A <sivananaf@gmail.com>
2021-04-28 14:53:33 +0300

    create an item model

---
+++ src/Model/TodoItem.js
@@ -0,0 +1,13 @@
+class TodoItemModel{
+    constructor(title){
+        this.title = title;
+        this.id = this.getRandomIntInclusive(1,100);
+        this.completed = false;        
+    }
+    getRandomIntInclusive(min,max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    }
+}
+export default TodoItemModel;
